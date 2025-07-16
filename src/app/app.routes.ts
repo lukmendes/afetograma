e7c21b3cb537436ace88f2588f4d10895b27bfbc
Lucas Mendes <lms252006@gmail.com>
2025-07-15 22:43:51 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
