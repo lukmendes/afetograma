8234154a977792a562d546a161b9c496c02f4bd2
Lucas Mendes <lms252006@gmail.com>
2025-07-15 22:44:24 -0300

    componente header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiService } from '../services/ui.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private ui: UiService) {}
 
+  scrollToSobre() {
+    document.getElementById('sobre')?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  irParaLogin(index: number) {
+    this.ui.setModo(index); // 0 = login, 1 = criar conta
+  }
 }
